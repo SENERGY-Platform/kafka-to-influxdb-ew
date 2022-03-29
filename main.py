b35f4cbd8206717a36ed9789f3e7bce166db1091
y-du <42994541+y-du@users.noreply.github.com>
2022-03-29 13:11:29 +0200

    allow missing config options

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ import signal
 
 if __name__ == '__main__':
     util.print_init(name="kafka-to-influxdb-ew", git_info_file="git_commit")
-    config = util.Config(prefix="conf", require_value=True)
+    config = util.Config(prefix="conf")
     util.init_logger(config.logger_level)
     util.logger.debug(f"export worker config: {config}")
     influxdb_client = influxdb.InfluxDBClient(
